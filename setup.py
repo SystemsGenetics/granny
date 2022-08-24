4d0022e2c7def333e909ab63681e6dbd0aca081a
nhanng1124 <nhan.h.nguyen@wsu.edu>
2022-08-24 00:00:48 -0700

    Added setup file for PyPI

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+requirements = """pandas
+ipython
+numpy
+opencv-python
+matplotlib
+h5py<3.0.0
+scikit-image
+tensorflow<=1.15
+pillow
+protobuf<=3.20
+keras==2.3
+""".split()
+    
+
+setuptools.setup(
+    name='granny',
+    packages=setuptools.find_packages(),
+    url="",
+    version='0.0.1',
+    description='GRANNY is an implementation of Mask-RCNN and image processing techniques,\
+        developed by the Ficklin Research Program, to rate disorder severity in Granny Smith apple.',
+    author='Nhan H Nguyen',
+    license='GNU General Public License v3.0',
+    python_requires='==3.7',
+    install_requires=requirements,
+    entry_points={'console_scripts': [
+        'granny = GRANNY.command:main',
+        'granny-gui = GRANNY.command:launch_gui',
+    ]},
+)
+
