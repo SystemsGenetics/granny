542055c0ab8423c22ad5c41a80bd238380e6d4ca
nhanng1124 <nhan.h.nguyen@wsu.edu>
2022-12-29 16:55:10 -0800

    Updated Python version requirements

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ setuptools.setup(
     name='granny',
     packages=setuptools.find_packages(),
     url="https://github.com/SystemsGenetics/granny",
-    version='1.0.4',
+    version='1.1.0',
     description='GRANNY is an implementation of Mask-RCNN and image processing techniques,\
         developed by the Ficklin Research Program, to rate disorder severity in "Granny Smith" apple.',
     author='Nhan H. Nguyen',
     license='GNU General Public License v3.0',
-    python_requires='<=3.7.15',
+    python_requires='<3.8',
     install_requires=[
         requirements,
     ],
