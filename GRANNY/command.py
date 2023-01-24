f6760f70375eda45b7d63481b9f83bb299abe998
nhanng1124 <nhan.h.nguyen@wsu.edu>
2023-01-24 12:45:16 -0800

    Updated CLI

--- GRANNY/command.py
+++ GRANNY/command.py
@@ -14,15 +14,16 @@ def main():
     parser.add_argument("-p", "--image_path", dest = "path", type = str, nargs = "?", 
     required = True, help = "Required. Specify a directory for --action==\"extract\" or a file for --action==\"rate\"")
 
-    parser.add_argument("-m", "--mode", dest = "mode", type = int, nargs = "?", 
-    required = False, help = "Optional. Specify 2 for multiples images processing when --action==\"rate\". There is no need to specify when extracting.")
+    parser.add_argument("-n", "--num_instances", dest = "num_instances", type = int, nargs = "?", 
+    required = False, help = "Optional. For --action \"extract\", specify how many number of instances you want to extract, by default it is 18.\
+    For --action \"rate\", specify 2 when passing in multiple images, by default it is 1 for single image processing.")
     
     parser.add_argument("-v", "--verbose", dest = "verbose", type = int, nargs = "?", 
     default = gn.VERBOSE, required = False, help = "Optional. Specify 0 to switch off model display.")
     
     args = parser.parse_args()
     
-    gn.setParameters(args.action, args.path, args.mode)
+    gn.setParameters(args.action, args.path, args.num_instances)
 
     gn.setVerbosity(args.verbose)
 
