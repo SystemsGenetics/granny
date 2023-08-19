b647c30efcd1d51e3b366224d22465f0b4aad562
nhanng1124 <nhan.h.nguyen@wsu.edu>
2023-08-19 16:35:39 -0700

    move Mask_RCNN

--- GRANNY/GRANNY_config.py
+++ GRANNY/GRANNY_config.py
@@ -1,11 +1,13 @@
-import sys 
 import os
 import pathlib
-sys.path.append(os.path.join(pathlib.Path(__file__).parent.resolve(), "Mask_RCNN"))
+import sys
+
+sys.path.append(os.path.join(pathlib.Path(__file__).parent.parent.resolve(), "Mask_RCNN"))
 import config as MRCNN_config
-import utils as MRCNN_utils
 import model as MRCNN_model
+import utils as MRCNN_utils
 import visualize as MRCNN_visualize
+
 
 class AppleConfig(MRCNN_config.Config):
     NUM_CLASSES = 1 + 1
@@ -13,4 +15,3 @@ class AppleConfig(MRCNN_config.Config):
     IMAGES_PER_GPU = 1
     GPU_COUNT = 1
     BATCH_SIZE = 1
-
