7fc5a729e2af771ce14f3e530f937f1f6a7ccfef
nhanng1124 <nhan.h.nguyen@wsu.edu>
2023-09-24 15:32:40 -0700

    rename starch function

--- GRANNY/GRANNY_StarchIndex.py
+++
@@ -1,9 +0,0 @@
-from GRANNY import GRANNY_Base as granny
-
-
-class GrannyStarchIndex(granny.GrannyBase):
-    def __init__(self, action:str , fname:str):
-        super(GrannyStarchIndex).__init__(action, fname)
-
-    def main() -> None:
-        pass
