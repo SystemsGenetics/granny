9ab1cb64ca1f597624c3081f2c66a0ddd3fda115
nhanng1124 <nhan.h.nguyen@wsu.edu>
2024-02-15 19:33:13 -0800

    fix import error old Granny

--- GRANNY/GRANNY_config.py
+++ GRANNY/GRANNY_config.py
@@ -2,12 +2,14 @@ import os
 import pathlib
 import sys
 
-from Mask_RCNN import config as MRCNN_config
-
 sys.path.append(
     os.path.join(pathlib.Path(__file__).parent.parent.resolve(), "Mask_RCNN")
 )
 
+import config as MRCNN_config
+import model as MRCNN_model
+import utils as MRCNN_utils
+import visualize as MRCNN_visualize
 
 class AppleConfig(MRCNN_config.Config):
     NUM_CLASSES = 1 + 1
