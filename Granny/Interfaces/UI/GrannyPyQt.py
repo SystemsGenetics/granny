afdd69ae587ee00582835ebd4ffbe15da67adeca
Loren Honaas <loren.honaas@usda.gov>
2026-07-31 12:45:35 -0700

    Add Quit button, clean exit handlers, and -i gui integration

--- Granny/Interfaces/UI/GrannyPyQt.py
+++ Granny/Interfaces/UI/GrannyPyQt.py
@@ -10,5 +10,16 @@ class GrannyPyQt(GrannyUI):
         """
         GrannyUI.__init__(self, parser)
 
-    def run(self):
+    def addProgramArgs(self):
         pass
+
+    def run(self):
+        import subprocess
+        import sys
+        import os
+        gui_path = os.path.expanduser('~/softwares/granny-gui/granny_gui.py')
+        python = sys.executable
+        try:
+            subprocess.run([python, gui_path])
+        except KeyboardInterrupt:
+            sys.exit(0)
